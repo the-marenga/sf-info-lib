17d01e3d825c42fc076505af36380c1b29ac9a9c
Marenga <marengamatdrb@gmail.com>
2025-06-01 17:29:56 +0200

    Bruh

--- migrations/20250531173433_ranks.sql
+++ migrations/20250531173433_ranks.sql
@@ -1,9 +1,6 @@
 -- Add migration script here
 AlTER TABLE player ADD column guild_id INT REFERENCES guild(guild_id);
 
-WITH bad_guilds AS (
-    SELECT guild_id FROM guild where name = ''
-)
 
 CREATE INDEX idx_player_server_active_honor ON player (server_id, honor DESC, player_id)
 INCLUDE (guild_id, name, level)
