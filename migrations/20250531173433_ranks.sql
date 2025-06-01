723e2d6a67f3da345b797202c10482eab9fd7595
Marenga <marengamatdrb@gmail.com>
2025-06-01 17:28:00 +0200

    Do guild_id update later

--- migrations/20250531173433_ranks.sql
+++ migrations/20250531173433_ranks.sql
@@ -7,15 +7,6 @@ WITH bad_guilds AS (
 UPDATE player_info
 SET guild_id = NULL WHERE guild_id in (SELECT guild_id FROM bad_guilds);
 
-UPDATE player
-SET guild_id = (
-    SELECT pi.guild_id
-    FROM player_info pi
-    WHERE pi.player_id = player.player_id
-    ORDER BY pi.fetch_time DESC
-    LIMIT 1
-);
-
 CREATE INDEX idx_player_server_active_honor ON player (server_id, honor DESC, player_id)
 INCLUDE (guild_id, name, level)
 WHERE honor IS NOT NULL AND is_removed = FALSE;
