bc1268f6575c35be69d94611a06c54da678d3962
Marenga <marengamatdrb@gmail.com>
2025-06-01 17:29:08 +0200

    Also delay the other update

--- migrations/20250531173433_ranks.sql
+++ migrations/20250531173433_ranks.sql
@@ -4,8 +4,6 @@ AlTER TABLE player ADD column guild_id INT REFERENCES guild(guild_id);
 WITH bad_guilds AS (
     SELECT guild_id FROM guild where name = ''
 )
-UPDATE player_info
-SET guild_id = NULL WHERE guild_id in (SELECT guild_id FROM bad_guilds);
 
 CREATE INDEX idx_player_server_active_honor ON player (server_id, honor DESC, player_id)
 INCLUDE (guild_id, name, level)
