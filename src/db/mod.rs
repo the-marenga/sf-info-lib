07743a7e0436e2bff6b55cc1bd718ad5d94ff19a
Marenga <marengamatdrb@gmail.com>
2025-06-01 22:45:53 +0200

    NVM

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -681,3 +681,18 @@ pub async fn get_hof_player(
         })
         .collect())
 }
+
+// WITH bad_guilds AS (
+//     SELECT guild_id FROM guild where name = ''
+// )
+// UPDATE player_info
+// SET guild_id = NULL WHERE guild_id in (SELECT guild_id FROM bad_guilds);
+
+// UPDATE player
+// SET guild_id = (
+//     SELECT pi.guild_id
+//     FROM player_info pi
+//     WHERE pi.player_id = player.player_id
+//     ORDER BY pi.fetch_time DESC
+//     LIMIT 1
+// );
