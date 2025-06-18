2e4ea5afd3aed59889e2c1a095626f1f4d9c8a0a
Marenga <marengamatdrb@gmail.com>
2025-06-18 12:57:53 +0200

    Also drop constraint

--- migrations/20250618104914_other_hash.sql
+++ migrations/20250618104914_other_hash.sql
@@ -1,4 +1,6 @@
 -- Add migration script here
+ALTER TABLE otherplayer_resp DROP CONSTRAINT otherplayer_resp_hash_key;
+
 DROP INDEX otherplayer_resp_hash_key;
 
 CREATE INDEX otherplayer_resp_hash_key on otherplayer_resp using HASH(hash);
