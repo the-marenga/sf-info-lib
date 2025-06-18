f54ecc070b9a3212f5d06c5ecec6a14f2b4fc9dc
Marenga <marengamatdrb@gmail.com>
2025-06-18 13:00:09 +0200

    Fix syntax

--- migrations/20250618104914_other_hash.sql
+++ migrations/20250618104914_other_hash.sql
@@ -1,6 +1,4 @@
 -- Add migration script here
 ALTER TABLE otherplayer_resp DROP CONSTRAINT otherplayer_resp_hash_key;
 
-DROP INDEX otherplayer_resp_hash_key;
-
 CREATE INDEX otherplayer_resp_hash_key on otherplayer_resp using HASH(hash);
