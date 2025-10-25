fb68781fe92bf1b3aaff738ff3b6cea8f4199794
Marenga <marengamatdrb@gmail.com>
2025-10-25 21:46:05 +0200

    Merge migration

--- migrations/20251025191105_slim_info.sql
+++ migrations/20251025191105_slim_info.sql
@@ -1,12 +1,8 @@
-drop INDEX player_info_idx;
+DROP INDEX IF EXISTS player_info_idx;
+
+ALTER TABLE player_info
+     DROP COLUMN xp,
+     ALTER COLUMN level TYPE int2 USING level::int2,
+     ALTER COLUMN soldier_advice TYPE int2 USING soldier_advice::int2;
+
 CREATE INDEX player_info_idx ON player_info (player_id);
-
-ALTER TABLE player_info drop column xp;
-
-ALTER TABLE player_info
-ALTER COLUMN level TYPE int2
-USING level::int2;
-
-ALTER TABLE player_info
-ALTER COLUMN soldier_advice TYPE int2
-USING soldier_advice::int2;
